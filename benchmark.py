040de31e5ad32a1904e176b4e55d45bd77e3391f
Keming <kemingyang@tensorchord.ai>
2023-01-18 10:57:44 +0800

    add v0
    Signed-off-by: Keming <kemingyang@tensorchord.ai>

--- benchmark.py
+++ benchmark.py
@@ -3,13 +3,16 @@ import os
 from time import perf_counter
 import subprocess
 from typing import List, Dict
+
 import pandas as pd
 import seaborn as sns
 
 
 COMMANDS: Dict[str, List[str]] = {
-    "envd-cpu": ["envd", "build"],
-    "envd-gpu": ["envd", "build", "-f", ":gpu_build"],
+    "envd-v0-cpu": ["envd", "build", "-f", "v0.envd:build"],
+    "envd-v0-gpu": ["envd", "build", "-f", "v0.envd:gpu_build"],
+    "envd-v1-cpu": ["envd", "build", "-f", "v1.envd:build"],
+    "envd-v1-gpu": ["envd", "build", "-f", "v1.envd:gpu_build"],
     "docker-cpu": ["docker", "buildx", "build", "-f", "Dockerfile", "."],
     "docker-gpu": ["docker", "buildx", "build", "-f", "gpu.Dockerfile", "."],
 }
@@ -28,8 +31,13 @@ def envd_version() -> str:
     return ver
 
 
-def record(cmd: List[str]) -> float:
+def record(name: str, cmd: List[str]) -> float:
     t0 = perf_counter()
+
+    # envd needs to bootstrap the buildkitd
+    if name.startswith("envd"):
+        subprocess.call(["envd", "bootstrap"])
+
     code = subprocess.call(cmd)
     if code != 0:
         print("ERROR: ", cmd)
@@ -46,29 +54,34 @@ def record(cmd: List[str]) -> float:
 
 def run() -> List[float]:
     res = []
-    for cmd in COMMANDS.values():
-        res.append(record(cmd))
+    for name, cmd in COMMANDS.items():
+        res.append(record(name, cmd))
     print(res)
     return res
 
 
 def render(data: pd.DataFrame):
     sns.set_theme(style="whitegrid")
-    ax = sns.lineplot(data=data, palette="tab10", linewidth=2.5)
-    ax.get_figure().savefig("benchmark.png")
+    ax = sns.lineplot(
+        data=data, palette="tab10", linewidth=2.5, markers=True, dashes=False
+    )
+    ax.get_figure().savefig("trend.png")
 
 
 def combine(record: List[float], path: str) -> pd.DataFrame:
     old = pd.DataFrame(columns=NAMES)
     if os.path.isfile(path):
         old = pd.read_csv(path)
-    data = pd.concat([old, pd.DataFrame([record], columns=NAMES)])
-    data.to_csv(path)
+    data = pd.concat(
+        [old, pd.DataFrame([record], columns=NAMES)], ignore_index=True, sort=False
+    )
+    data.to_csv(path, index=False)
     return data
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    res = run()
+    # res = run()
+    res = [10, 20, 30, 40]
     data = combine(res, args.path)
     render(data)
